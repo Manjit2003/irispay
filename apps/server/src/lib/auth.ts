066d1a68d5d953555e34b9ea4977cf002e23c35b
Manjit Pardeshi <manjitpardeshi91991@gmail.com>
2025-06-13 01:47:10 +0530

    chore: init

---
+++ apps/server/src/lib/auth.ts
@@ -0,0 +1,27 @@
+
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { expo } from "@better-auth/expo";
+import { db } from "../db";
+import * as schema from "../db/schema/auth";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    
+    provider: "sqlite",
+    
+    schema: schema,
+  }),
+  trustedOrigins: [
+    process.env.CORS_ORIGIN || "",
+    "my-better-t-app://",
+  ],
+  emailAndPassword: {
+    enabled: true,
+  }
+
+  ,
+  plugins: [expo()]
+});
+
+
